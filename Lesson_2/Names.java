b272941ef1b5b9d7b60b10d161ba2c3142ed04c1
Darya Andreyeva <Darya_Andreyeva@rcomext.com>
2019-08-12 19:04:30 +0300

    adjusted as per comment

--- Lesson_2/Names.java
+++ Lesson_2/Names.java
@@ -3,25 +3,18 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
 public class Names {
-    public static String name(Scanner name) {
-        System.out.println("Enter your name: ");
-        String userName = name.nextLine();
-        return userName;
-    }
-
-    public static String surname(Scanner surname) {
-        System.out.println("Enter your surname: ");
-        String userSurname = surname.nextLine();
-        return userSurname;
-    }
-
+    
     public static void main(String[] args) {
-        Scanner names = new Scanner(System.in);
-        String name = name(names);
-        String surname = surname(names);
+		Scanner names = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+		String name = names.nextLine();
+		System.out.println("Enter your surname: ");
+        String surname = names.nextLine();
         System.out.println(name + " " + surname + ", lesson 2 is finished");
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(dtf.format(now));
     }
 }
+
+
